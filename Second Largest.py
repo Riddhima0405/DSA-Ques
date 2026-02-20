dfa0b7f6db6f29361942c51c6898addcacdb58ce
Riddhima0405 <181880323+Riddhima0405@users.noreply.github.com>
2026-02-20 22:53:23 +0530

    Add function to find second largest number
    Implement logic to find the second largest number in an array.

---
+++ Second Largest.py
@@ -0,0 +1,13 @@
+largest = float('-inf')
+    second_largest = float('-inf')
+
+    for num in arr:
+        if num > largest:
+            second_largest = largest
+            largest = num
+        elif num > second_largest and num != largest:
+            second_largest = num
+
+    if second_largest == float('-inf'):
+        return -1   
+    return second_largest
